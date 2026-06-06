94a3a6cd6be2dc997d47fb113b222cceae1fd8b7
naddhiif <nadhifqonita4@gmail.com>
2026-06-06 17:08:31 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   if request.method == 'POST':
+      namaDepan = request.form['namaDepan']
+      namaBelakang = request.form['namaBelakang']
+      nama = '%s %s' % (namaDepan, namaBelakang)
+      return render_template('response.html', nama=nama)
+   return render_template('form.html')
+
+if __name__ == '__main__':
+   app.run(debug=True)
